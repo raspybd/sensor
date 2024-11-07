f264839d00f7eb09e44fad415a20867c5663ddd7
raspybd <raspybd@gmail.com>
2024-11-07 19:06:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(express.static('./'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sens.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
